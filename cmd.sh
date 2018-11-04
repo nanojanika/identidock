d952c375a370afeb88c49a654af10e6b87037dbf
root <heino.talvik@gmail.com>
2018-11-04 16:18:17 +0000

    First commit

---
+++ cmd.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+if [ "$ENV" = 'DEV' ]; then
+  echo "Running Development Server" # Start server for development
+  exec python "/app/identidock.py"
+elif [ "$ENV" = 'UNIT' ]; then
+  echo "Running Unit Tests"
+  exec python "test.py"
+else
+  echo "Running Production Server" # Start server for production
+  exec uwsgi --http 0.0.0.0:9090 --wsgi-file /app/identidock.py \
+             --callable app --stats 0.0.0.0:9191
+fi
