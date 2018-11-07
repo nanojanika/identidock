89d52dc7389a847c80d53938ad5e80469fb8bd63
Heino Talvik <heino.talvik@gmail.com>
2018-11-07 17:46:00 +0200

    Update test.py

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ class TestCase(unittest.TestCase):
      page = self.app.post("/", data=dict(name="Moby Dock"))
      assert page.status_code == 200
      assert 'Hello' in str(page.data)
-     assert 'Moby Dock' in str(page.data)
+     assert 'Heino' in str(page.data)
   def test_html_escaping(self):
      page = self.app.post("/", data=dict(name='"><b>TEST</b><!--'))
      assert '<b>' not in str(page.data)
